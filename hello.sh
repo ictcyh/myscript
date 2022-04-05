e265537d970a2f6f7886c267b72cc5b5390867f3
ictcyh <35756424+ictcyh@users.noreply.github.com>
2022-04-05 16:16:06 +0900

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -7,7 +7,7 @@ git clone https://github.com/ictcyh/myscript
 cd myscript
 ./hello.sh
 
-  sudo yum update -y
-  sudo amazon-linux-extras install docker
-  sudo service docker start
+sudo yum update -y
+sudo amazon-linux-extras install docker
+sudo service docker start
  
